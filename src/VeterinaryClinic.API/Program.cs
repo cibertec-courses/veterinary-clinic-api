d344f91aabf6cbdabc40f411a6662627ea504b08
Christiam Mendoza <ch.alb.rz@gmail.com>
2025-12-13 16:03:21 -0500

    MIddleware complete

--- src/VeterinaryClinic.API/Program.cs
+++ src/VeterinaryClinic.API/Program.cs
@@ -1,44 +1,48 @@
 using DotNetEnv;
+using VeterinaryClinic.API;
 using VeterinaryClinic.Application;
 using VeterinaryClinic.Infrastructure;
 
-
-
-var currentDir = Directory.GetCurrentDirectory();
-var envPath = Path.Combine(currentDir, ".env");
-if(!File.Exists(envPath))
+var envPath = Path.Combine(Directory.GetCurrentDirectory(), ".env");
+if (!File.Exists(envPath))
 {
-    envPath = Path.Combine(currentDir, "..", "..", ".env");
-    
+    envPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", ".env");
 }
+
 if (File.Exists(envPath))
 {
     Env.Load(envPath);
-    Console.WriteLine($".env file loaded from: {envPath}");
-    Console.WriteLine(envPath.ToString());
-}else
-{
-    Console.WriteLine(".env file not found.");
+    Console.WriteLine($"Loaded .env from: {envPath}");
 }
-
-
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configurar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddInfrastructure();
 builder.Services.AddApplication();
 
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseMiddleware<GlobalExceptionMiddleware>();
+
+
+// Usar CORS antes de otros middlewares
+app.UseCors("AllowFrontend");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -46,9 +50,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
