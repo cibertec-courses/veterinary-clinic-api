927e5000fe323fbd2f7633865cdea80ec5049053
Christiam Mendoza <ch.alb.rz@gmail.com>
2025-12-16 19:50:42 -0500

    Add DTOs and validators for authentication (AuthResponseDto, LoginDto, RegisterDto)

---
+++ src/VeterinaryClinic.Application/DTOs/Auth/RegisterDto.cs
@@ -0,0 +1,14 @@
+
+namespace VeterinaryClinic.Application.DTOs.Auth
+{
+    public class RegisterDto
+    {
+        
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Role { get; set; } = "Receptionnist"; // e.g., Admin
+    }
+
+}
